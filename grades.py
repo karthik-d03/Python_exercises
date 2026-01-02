96b2215d2df85d38392c205d1ff3e837875a73c6
karthik-d03 <karthik.d03.m@gmail.com>
2026-01-02 14:24:47 +0530

    if_elif_change_order

--- grades.py
+++ grades.py
@@ -1,7 +1,7 @@
 max = 100 * 3
-eng = 85
-math = 85
-sci = 85
+eng = 75
+math = 75
+sci = 75
 
 percent = (eng + math + sci) / 3
 
@@ -9,7 +9,7 @@ if percent > 90 :
     print("Grade A") 
 elif (percent > 80) :
     print("grade B")
-elif (percent >= 35) :
-    print("Avg")
 elif percent < 35 :
     print("Fail")
+elif (percent >= 35) :
+    print("Avg")
